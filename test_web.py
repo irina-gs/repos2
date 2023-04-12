1ff901ed49f25eb0f475789c580739653486b255
Guskova Irina <78543273+irina-gs@users.noreply.github.com>
2023-04-12 15:24:10 +0300

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -5,5 +5,5 @@ import pytest
 @pytest.mark.usefixtures("setup")
 class Tests:
     def test_title(self):
-        self.driver.get(os.getenv('p.mrsu.ru'))
+        self.driver.get(os.getenv('https://p.mrsu.ru'))
         assert self.driver.title == "Вход - ЭИОС"
